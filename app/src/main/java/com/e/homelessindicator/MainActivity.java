65609dbbc67c19876a0fc191eba8cf349c46ac2b
Vitor Stahelin <vitorjosestahelin@gmail.com>
2019-07-13 22:43:04 -0300

    Adding

--- app/src/main/java/com/e/homelessindicator/MainActivity.java
+++ app/src/main/java/com/e/homelessindicator/MainActivity.java
@@ -3,6 +3,8 @@ package com.e.homelessindicator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void insertPoint(View v){
+        Toast.makeText(MainActivity.this, "Teste", Toast.LENGTH_LONG).show();
     }
 }
