f387fef2301e07eeb08b820ef2ed286a0b4eb5a9
Vitor Stahelin <vitorjosestahelin@gmail.com>
2019-07-14 14:12:12 -0300

    Implement Gsheets for Db, implement manual adiciton

--- app/src/main/java/com/e/homelessindicator/MainActivity.java
+++ app/src/main/java/com/e/homelessindicator/MainActivity.java
@@ -1,20 +1,35 @@
 package com.e.homelessindicator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button buttonAddItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAddItem = (Button)findViewById(R.id.btn_add_item);
+        buttonAddItem.setOnClickListener(this);
     }
 
-    public void insertPoint(View v){
-        Toast.makeText(MainActivity.this, "Teste", Toast.LENGTH_LONG).show();
+    public void buttonTest(View v){
+        Toast.makeText(MainActivity.this, "In progress", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonAddItem){
+
+            Intent intent = new Intent(getApplicationContext(),AddItem.class);
+            startActivity(intent);
+        }
     }
 }
